b89fcbd59e119fa17b6909fcaa430065d3980000
stacie.taylor <stacie.taylor@zapier.com>
2021-12-21 21:23:33 -0800

    Fix responsive styling on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { mediaQueries } from "../styles/mediaQueries";
 
 export const Footer = () => {
   return (
@@ -31,19 +32,26 @@ const Outer = styled.div`
 `;
 
 const ContentContainer = styled.div`
-  max-width: 1200px;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  width: 100%;
-  padding: 10px 20px;
+  padding: 20px;
+  justify-items: center;
+  ${mediaQueries.desktop} {
+    max-width: 1200px;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    width: 100%;
+  }
 `;
 
 const QuoteContainer = styled.div`
-  display: grid;
-  grid-template-columns: max-content auto;
-  align-items: center;
-  grid-gap: 5px;
+  padding-bottom: 12px;
+  ${mediaQueries.desktop} {
+    display: grid;
+    grid-template-columns: max-content auto;
+    align-items: center;
+    grid-gap: 5px;
+    padding-bottom: 0;
+  }
 `;
 
 const Quote = styled.small`
@@ -54,18 +62,24 @@ const Quote = styled.small`
 `;
 
 const HeartIcon = styled.img`
-  filter: invert(1);
-  height: 20px;
+  display: none;
+  ${mediaQueries.desktop} {
+    display: block;
+    filter: invert(1);
+    height: 20px;
+  }
 `;
 
 const SocialMediaIconGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  justify-self: self-end;
-  grid-column-end: none;
   width: max-content;
   grid-column-gap: 20px;
   padding-top: 7px;
+  ${mediaQueries.desktop} {
+    justify-self: self-end;
+    grid-column-end: none;
+  }
 `;
 
 const SocialMediaIcon = styled.img`
