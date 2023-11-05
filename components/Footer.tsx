d40e925268b0d25ee201efe8250b987068151ecf
stacie.taylor <stacie.taylor@zapier.com>
2023-11-05 13:43:40 -0800

    Move content to footer and make typography color variable

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,33 @@
 import styled from "@emotion/styled";
 import { mediaQueries } from "../styles/mediaQueries";
+import { H2, Paragraph } from "./Typography";
 
 export const Footer = () => {
   return (
     <Outer>
       <ContentContainer>
-        <QuoteContainer>
-          <Quote>take care of each other, okay.</Quote>{" "}
-          <HeartIcon src="heart.png" alt="heart" height={50} />
-        </QuoteContainer>
-        <SocialMediaIconGrid>
-          <a href="https://twitter.com/the_real_Stacie">
-            <SocialMediaIcon src="twitter.png" alt="twitter" height={50} />
-          </a>
-          <a href="https://www.linkedin.com/in/therealstacie/">
-            <SocialMediaIcon src="linkedin.png" alt="linkedin" height={50} />
-          </a>
-          <a href="https://www.instagram.com/the_real_stacie/">
-            <SocialMediaIcon src="instagram.png" alt="instagram" height={50} />
-          </a>
-        </SocialMediaIconGrid>
+        <H2 id="contact-footer" color={'#FAF9F6'} >get in touch.</H2>
+        <Paragraph color={'#FAF9F6'}>
+          I am absolutely driven by human connection and always eager to
+          meet a new friend. Hit me up if you ever want to chat about
+          leadership, diversity and inclusion, engineering, parenting,
+          wine & cheese, or really anything femme.
+        </Paragraph>
       </ContentContainer>
+      <SocialMediaContainer>
+          <Quote>take care of each other, okay.</Quote>{" "}
+          <SocialMediaIconGrid>
+            <a href="https://twitter.com/the_real_Stacie">
+              <SocialMediaIcon src="twitter.png" alt="twitter" height={50} />
+            </a>
+            <a href="https://www.linkedin.com/in/therealstacie/">
+              <SocialMediaIcon src="linkedin.png" alt="linkedin" height={50} />
+            </a>
+            <a href="https://www.instagram.com/the_real_stacie/">
+              <SocialMediaIcon src="instagram.png" alt="instagram" height={50} />
+            </a>
+          </SocialMediaIconGrid>
+        </SocialMediaContainer>
     </Outer>
   );
 };
@@ -29,29 +36,27 @@ const Outer = styled.div`
   background-color: #000000b5;
   display: grid;
   place-items: center;
+  ${mediaQueries.desktop}{
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 
 const ContentContainer = styled.div`
   display: grid;
   padding: 20px;
-  justify-items: center;
   ${mediaQueries.desktop} {
     max-width: 1200px;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
     width: 100%;
   }
 `;
 
-const QuoteContainer = styled.div`
-  padding-bottom: 12px;
-  ${mediaQueries.desktop} {
-    display: grid;
-    grid-template-columns: max-content auto;
-    align-items: center;
-    grid-gap: 5px;
-    padding-bottom: 0;
-  }
+const SocialMediaContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  place-content: center;
+  border-style: solid;
+  border-color: #FAF9F6;
+  width: 80%;
 `;
 
 const Quote = styled.small`
@@ -59,15 +64,6 @@ const Quote = styled.small`
   font-size: 16px;
   font-family: Tiempos Headline, Georgia, serif;
   text-transform: uppercase;
-`;
-
-const HeartIcon = styled.img`
-  display: none;
-  ${mediaQueries.desktop} {
-    display: block;
-    filter: invert(1);
-    height: 20px;
-  }
 `;
 
 const SocialMediaIconGrid = styled.div`
